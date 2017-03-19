39cd88ad6eceed1711bdfdbba622795226e4862e
Cédric Jeanneret <tengu@ethack.org>
2017-03-19 12:18:48 +0100

    added missing domain

--- docker-entrypoint.d/00-configure.sh
+++ docker-entrypoint.d/00-configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "x${FCGI_PORT}" != "x" ]; then
   remote_host=$(curl http://169.254.169.250/latest/self/stack/services/application/containers/0/name)
-  echo "upstream catalyst { server ${remote_host}:${FCGI_PORT}; }" > /etc/nginx/conf.d/upstream.conf
+  echo "upstream catalyst { server ${remote_host}.rancher.internal:${FCGI_PORT}; }" > /etc/nginx/conf.d/upstream.conf
 else
   echo 'Nothing to do - no FCGI_PORT in env'
 fi
